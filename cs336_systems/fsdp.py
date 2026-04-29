b7eb8c80a725248fd489e845c922c872f5d9bbd3
SKAssist <sarakothari@gmail.com>
2026-04-29 04:00:42 -0700

    passing all tests

--- cs336_systems/fsdp.py
+++ cs336_systems/fsdp.py
@@ -34,26 +34,41 @@ class FSDP(nn.Module):
             
         
         def all_gather_forward_pre_hook(layer, inputs):
-            
+            if (layer.all_gathered):
+                    return
+            layer.all_gathered = True
             full= torch.empty(layer.weight.shape[0]*self.world_size, *layer.weight.shape[1:], device=layer.weight.device, dtype=torch.float32)
             dist.all_gather_into_tensor(full, layer.weight.data)
             layer.original_shard = layer.weight.data
             if compute_dtype is not None:
                 full = full.to(compute_dtype)
             layer.weight.data = full
-            print("okay gather")
+        #     print("okay gather")
             if compute_dtype is not None and not isinstance(layer, (Embedding, nn.Embedding)):
                 return (inputs[0].to(compute_dtype),)
         
+        def all_gather_post_hook(idx):
+                def hook(layer, inputs, output):
+                        if idx + 2 < len(self.sharded_layers):
+                                target = self.sharded_layers[idx + 2]
+                                all_gather_forward_pre_hook(target, inputs)
+                return hook
+                
         
+        cur_index = 0
         for name, layer in self.module.named_modules():
             if isinstance(layer, (Linear, Embedding, nn.Linear, nn.Embedding)):
                 shard = shard_param(layer.weight,self.rank, self.world_size)
                 layer.weight = nn.Parameter(shard)
+                layer.all_gathered = False
                 self.sharded_layers.append(layer)
                 layer.register_forward_pre_hook(all_gather_forward_pre_hook)
+                layer.register_forward_hook(all_gather_post_hook(cur_index))
+                cur_index +=1
         
     def forward(self,*inputs,**kwargs):
+        for layer in self.sharded_layers:
+                layer.all_gathered = False
         return self.module.forward(*inputs, **kwargs)
     
     def finish_gradient_synchronization(self):
@@ -65,28 +80,28 @@ class FSDP(nn.Module):
             if layer.weight.grad is None:
                 layer.weight.data = layer.original_shard
                 del layer.original_shard
-                print("no grad")
+                # print("no grad")
                 return 
             full_grad = layer.weight.grad
             shard_grad = torch.empty(full_grad.shape[0]//self.world_size, *full_grad.shape[1:], device=layer.weight.device, dtype=layer.weight.dtype)
             dist.reduce_scatter_tensor(shard_grad, full_grad)
             layer.weight.data = layer.original_shard
             del layer.original_shard
-            print("layer weight", layer.weight.data.shape)
-            print("layer", layer.weight.grad.shape)
-            print(shard_grad.shape)
+        #     print("layer weight", layer.weight.data.shape)
+        #     print("layer", layer.weight.grad.shape)
+        #     print(shard_grad.shape)
             shard_grad = shard_grad / self.world_size
             layer.weight.grad = shard_grad.to(torch.float32)
-            print("okay reduce")
+        #     print("okay reduce")
             
         for layer in self.sharded_layers:
             
             if layer.weight.grad is None:
-                print("no grad in sync")
+                # print("no grad in sync")
                 continue
             
             reduce_scatter_backward_post_hook(layer, None, None)
-            print("layer weight data in sync", layer.weight.data.shape, "layer grad shape", layer.weight.grad.shape)
+        #     print("layer weight data in sync", layer.weight.data.shape, "layer grad shape", layer.weight.grad.shape)
         
         
         sharded_params = set(id(l.weight) for l in self.sharded_layers)
